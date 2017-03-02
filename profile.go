44b76efaa539238d58e596a7d9275d5a248fda06
Abdul Dakkak <abduld@wolfram.com>
2017-03-02 11:38:14 -0600

    allow .rai_profile

--- profile.go
+++ profile.go
@@ -54,15 +54,29 @@ func init() {
 
 		appName := config.App.Name
 
+		// load ~/.rai_profile
+		homeProfileFile := filepath.Join(homeDir, "."+appName+"_profile")
+		if com.IsFile(homeProfileFile) {
+			DefaultProfilePath = homeProfileFile
+			return
+		}
+
+		// load ~/.rai_env
+		homeEnvFile := filepath.Join(homeDir, "."+appName+"_env")
+		if com.IsFile(homeEnvFile) {
+			DefaultProfilePath = homeEnvFile
+			return
+		}
+
 		// load ~/.rai.profile
-		homeProfileFile := filepath.Join(homeDir, "."+appName+".profile")
+		homeProfileFile = filepath.Join(homeDir, "."+appName+".profile")
 		if com.IsFile(homeProfileFile) {
 			DefaultProfilePath = homeProfileFile
 			return
 		}
 
 		// load ~/.rai.env
-		homeEnvFile := filepath.Join(homeDir, "."+appName+".env")
+		homeEnvFile = filepath.Join(homeDir, "."+appName+".env")
 		if com.IsFile(homeEnvFile) {
 			DefaultProfilePath = homeEnvFile
 			return
